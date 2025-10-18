6f7a10e11f6bd3395abd4b9103fc62154943c68f
Antony Cabeza <antonycabeza@gmail.com>
2025-10-18 11:25:48 -0300

    Modificando el home

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
-        Reusmen
-      </main>
-
+    <div className="min-h-screen bg-gray-100 text-gray-800 font-sans leading-relaxed p-5">
+      <div className="max-w-3xl mx-auto bg-white p-8 md:p-10 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-semibold text-center text-[#2c3e50] border-b-2 border-[#3498db] pb-3 mt-6">
+          Resumen para el 2do Parcial de IPC - UBA XXI
+        </h1>
+{/* 
+        <p className="mt-6 mb-4 text-justify">
+          Este capítulo explora la <strong className="text-[#3498db]">perspectiva clásica</strong> de la filosofía de la ciencia, que abarca principalmente al{" "}
+          <strong className="text-[#3498db]">positivismo lógico</strong> y al{" "}
+          <strong className="text-[#3498db]">falsacionismo</strong>. Ambas corrientes, aunque con respuestas diferentes,
+          comparten una agenda de problemas comunes y un enfoque centrado en la reconstrucción lógica de las teorías
+          científicas y su relación con la experiencia.
+        </p> */}
+      </div>
     </div>
   );
 }
