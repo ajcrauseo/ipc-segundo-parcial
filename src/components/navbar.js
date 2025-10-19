d269d587df682c9ede6a77791b9def0fb838f452
Antony Cabeza <antonycabeza@gmail.com>
2025-10-18 23:19:08 -0300

    Fijando Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="bg-white shadow-md w-full z-10 top-0 left-0">
+    <nav className="bg-white shadow-md w-full z-10 top-0 left-0 fixed">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Título */}
